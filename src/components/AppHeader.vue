ce22e31e05432a737ff23d3e6662f11f2087fabe
Patryk Smakosz <patryksmakosz1@gmail.com>
2022-02-17 01:10:13 +0100

    add: base testing structure

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button v-show="loggedIn">Logout</button>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      loggedIn:false
+    }
+  }
+}
+</script>
---
+++ tests/unit/AppHeader.spec.js
@@ -0,0 +1,11 @@
+import AppHeader from '@/components/AppHeader'
+
+describe('AppHeader', () => {
+  test('If user not logged in, dont show the button',()=> {
+    expect(true).toBe(true)
+  })
+
+  test('If user logged in, show the button',()=> {
+    expect(true).toBe(true)
+  })
+})
