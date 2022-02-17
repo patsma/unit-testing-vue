c4492b6abbf75b9b4b99076aba7d832846d47684
Patryk Smakosz <patryksmakosz1@gmail.com>
2022-02-17 15:57:06 +0100

    add: RandomNumber tests; refactor: overall cleaning, semicolons

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -1,15 +1,15 @@
-import AppHeader from '@/components/AppHeader'
-import {mount} from '@vue/test-utils'
+import AppHeader from "@/components/AppHeader";
+import { mount } from "@vue/test-utils";
 
-describe('AppHeader', () => {
-  test('If user not logged in, dont show the button',()=> {
-    const wrapper = mount(AppHeader)
-    expect(wrapper.find('button').isVisible()).toBe(false)
-  })
+describe("AppHeader", () => {
+  test("If user not logged in, dont show the button", () => {
+    const wrapper = mount(AppHeader);
+    expect(wrapper.find("button").isVisible()).toBe(false);
+  });
 
-  test('If user logged in, show the button',async()=> {
-    const wrapper = mount(AppHeader)
-    await wrapper.setData({loggedIn: true})
-    expect(wrapper.find('button').isVisible()).toBe(true)
-  })
-})
+  test("If user logged in, show the button", async () => {
+    const wrapper = mount(AppHeader);
+    await wrapper.setData({ loggedIn: true });
+    expect(wrapper.find("button").isVisible()).toBe(true);
+  });
+});
