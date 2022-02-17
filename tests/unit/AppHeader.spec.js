193f9a6500689989069071a3b625e51025ce73ba
Patryk Smakosz <patryksmakosz1@gmail.com>
2022-02-17 01:29:11 +0100

    add: test for user logged in or not logged in

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -1,11 +1,15 @@
 import AppHeader from '@/components/AppHeader'
+import {mount} from '@vue/test-utils'
 
 describe('AppHeader', () => {
   test('If user not logged in, dont show the button',()=> {
-    expect(true).toBe(true)
+    const wrapper = mount(AppHeader)
+    expect(wrapper.find('button').isVisible()).toBe(false)
   })
 
-  test('If user logged in, show the button',()=> {
-    expect(true).toBe(true)
+  test('If user logged in, show the button',async()=> {
+    const wrapper = mount(AppHeader)
+    await wrapper.setData({loggedIn: true})
+    expect(wrapper.find('button').isVisible()).toBe(true)
   })
 })
